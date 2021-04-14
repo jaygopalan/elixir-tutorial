97ffe67e0e1cfeb56149a3d89713d9b6107453bb
jaygopalan <jay@jaygopalan.com>
2021-04-14 11:47:40 -0400

    put in umbrella

---
+++ apps/kv/lib/kv/bucket.ex
@@ -0,0 +1,33 @@
+defmodule KV.Bucket do
+  use Agent, restart: :temporary
+
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end) 
+  end
+
+  @doc """
+  Puts the `val` for the given `key` in `bucket`.
+  """
+  def put(bucket, key, val) do
+    Agent.update(bucket, &Map.put(&1, key, val))
+  end
+
+  @doc """
+  Gets a value from the `bucket` by `key`.
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Deletes `key` from `bucket`.
+
+  Returns the current value of `key`, if `key` exists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+end
