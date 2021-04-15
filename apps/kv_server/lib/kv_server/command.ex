d034067bcc9473d09cdef695628e6416a4bfbfd8
jaygopalan <jay@jaygopalan.com>
2021-04-15 17:50:15 -0400

    testing suite

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -4,13 +4,79 @@ defmodule KVServer.Command do
 
   ## Examples
 
-    iex> KVServer.Command.parse("CREATE shopping\r\n")
+    iex> KVServer.Command.parse "CREATE shopping\r\n"
+    {:ok, {:create, "shopping"}}
+    
+    iex> KVServer.Command.parse "CREATE  shopping \r\n"
     {:ok, {:create, "shopping"}}
 
+    iex> KVServer.Command.parse "PUT shopping milk 1\r\n"
+    {:ok, {:put, "shopping", "milk", "1"}}
+
+    iex> KVServer.Command.parse "GET shopping milk\r\n"
+    {:ok, {:get, "shopping", "milk"}}
+
+    iex> KVServer.Command.parse "DELETE shopping eggs\r\n"
+    {:ok, {:delete, "shopping", "eggs"}}
+
+  Unknown commands or commands with the wrong number of arguments return an error:
+
+    iex> KVServer.Command.parse "UNKNOWN shopping eggs\r\n"
+    {:error, :unknown_command}
+
+    iex> KVServer.Command.parse "GET shopping\r\n"
+    {:error, :unknown_command}
+
   """
-  def parse(_line) do
+  def parse(line) do
     case String.split(line) do
       ["CREATE", bucket] -> {:ok, {:create, bucket}}
+      ["PUT", bucket, key, val] -> {:ok, {:put, bucket, key, val}}
+      ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
+      ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
+      _ -> {:error, :unknown_command}
+    end
+  end
+
+  @doc """
+  Runs the given command.
+  """
+  def run(command, pid)
+
+  def run(command) do
+    run(command, KV.Registry)
+  end
+
+  def run({:create, bucket}, pid) do
+    KV.Registry.create(pid, bucket)
+    {:ok, "OK\r\n"}
+  end
+
+  def run({:get, bucket, key}, pid) do
+    lookup(bucket, fn pid ->
+      value = KV.Bucket.get(pid, key)
+      {:ok, "#{value}\r\nOK\r\n"}
+    end, pid)
+  end
+
+  def run({:put, bucket, key, value}, pid) do
+    lookup(bucket, fn pid ->
+      KV.Bucket.put(pid, key, value)
+      {:ok, "OK\r\n"}
+    end, pid)
+  end
+
+  def run({:delete, bucket, key}, pid) do
+    lookup(bucket, fn pid ->
+      KV.Bucket.delete(pid, key)
+      {:ok, "OK\r\n"}
+    end, pid)
+  end
+
+  defp lookup(bucket, callback, pid) do
+    case KV.Registry.lookup(pid, bucket) do
+      {:ok, pid} -> callback.(pid)
+      :error -> {:error, :not_found}
     end
   end
 end
--- apps/kv_server/test/kv_server/command_test.exs
+++ apps/kv_server/test/kv_server/command_test.exs
@@ -1,4 +1,28 @@
 defmodule KVServer.CommandTest do
   use ExUnit.Case, async: true
   doctest KVServer.Command
+
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{pid: context.test}
+  end
+
+  test "spawns buckets", %{pid: pid} do
+    # Create bucket
+    assert KVServer.Command.run({:create, "shopping"}, pid) == {:ok, "OK\r\n"}
+
+    # Put key in bucket
+    assert KVServer.Command.run({:put, "shopping", "milk", 1}, pid) == {:ok, "OK\r\n"}
+
+    # Get from bucket that exists
+    assert KVServer.Command.run({:get, "shopping", "milk"}, pid) == {:ok, "1\r\nOK\r\n"}
+  end
+
+  test "query nonexistent bucket", %{pid: pid} do
+    # Get from bucket that doesn't exist
+    assert KVServer.Command.run({:get, "shopping", "milk"}, pid) == {:error, :not_found}
+
+    # Put in bucket that doesn't exist
+    assert KVServer.Command.run({:put, "shopping", "milk", 1}, pid) == {:error, :not_found}
+  end
 end
